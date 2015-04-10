b303d34b4d0b06bb0c396452a17d36f3a3925322
Ramon Ackermann <r.ackermann@uselab.com>
2015-04-10 16:19:46 +0200

    initial commit

---
+++ src/Sboo/Laravel5Mailjet/Laravel5MailjetServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace Sboo\Laravel5Mailjet;
+
+use Illuminate\Support\ServiceProvider;
+
+class Laravel5MailjetServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return [];
+	}
+
+}
